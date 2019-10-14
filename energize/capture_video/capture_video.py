80f593f9721ac054b02bed8fc268bd0cc3bd9b80
Faab <fabian.jansen@cblab.int>
2019-10-14 13:36:02 +0200

    Update

--- energize/capture_video/capture_video.py
+++ energize/capture_video/capture_video.py
@@ -1,16 +1,18 @@
-import datetime
 from energize.energy_prediction import energy_prediction
+from energize.utils import utils
+import datetime
 import cv2
 from PIL import ImageGrab
 import numpy as np
+import json
 
 
-def capture_video():
+def capture_video(conn):
     print("-- RUNNING Capture Video")
     while True:
         im = cv2.cvtColor(np.array(ImageGrab.grab()), cv2.COLOR_RGB2BGR)
-        energy_prediction.main(im, datetime.datetime.now())
+        conn(im)
 
 
 if __name__ == "__main__":
-    capture_video()
+    capture_video(energy_prediction.main)
--- energize/main.py
+++ energize/main.py
@@ -1,4 +1,5 @@
 from energize.capture_video.capture_video import capture_video
+from energize.energy_prediction import energy_prediction
 import argparse
 
 if __name__ == "__main__":
@@ -6,4 +7,6 @@ if __name__ == "__main__":
     parser.add_argument("-v", action="store_true", help="Set verbosity")
     args = vars(parser.parse_args())
     verbose = args['v']
-    capture_video()
+
+
+    capture_video(energy_prediction.main)
