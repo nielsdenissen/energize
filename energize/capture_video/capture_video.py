f28b9f32a10a90b497d489eaf56bff40625a6aff
Niels Denissen <nielsdenissen@gmail.com>
2019-10-14 11:52:32 +0200

    Fix capture

--- energize/capture_video/capture_video.py
+++ energize/capture_video/capture_video.py
@@ -4,7 +4,8 @@ from energize.energy_prediction import energy_prediction
 
 def main():
     print("-- RUNNING Capture Video")
-    energy_prediction.main(datetime.datetime.now(), "empty_image")
+    # TODO: replace empty_image with actual image (representation by opencv probably)
+    energy_prediction.main("empty_image", datetime.datetime.now())
 
 
 if __name__ == "__main__":
