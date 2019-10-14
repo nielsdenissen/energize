f869e13ec704021c0fcd4c5ff8a5e3a62a0521d2
Faab <fabian.jansen@cblab.int>
2019-10-14 13:16:27 +0200

    Add argparse

--- energize/main.py
+++ energize/main.py
@@ -1,5 +1,9 @@
 from energize.capture_video.capture_video import capture_video
+import argparse
 
 if __name__ == "__main__":
-
+    parser = argparse.ArgumentParser(description='Energize')
+    parser.add_argument("-v", action="store_true", help="Set verbosity")
+    args = vars(parser.parse_args())
+    verbose = args['v']
     capture_video()
