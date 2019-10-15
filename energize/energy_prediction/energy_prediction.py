6e8e8eac33a989ac0f1c8516646740ae8e55625e
Faab <fabian.jansen@cblab.int>
2019-10-15 13:42:36 +0200

    First predict_energy

--- energize/energy_prediction/energy_prediction.py
+++ energize/energy_prediction/energy_prediction.py
@@ -1,15 +1,18 @@
+import cv2
+import numpy as np
 from energize.energy_prediction.read_expressions import ReadExpressions
 from energize.energy_prediction.compare_faces import CompareFaces
 from energize.energy_prediction.find_faces import FindFaces
 
-find_faces = FindFaces()
+read_expressions = ReadExpressions()
+compare_faces = CompareFaces(faces="/Users/faab/Desktop/energize/energize/known_faces2.npz", tolerance=0.7)
+find_faces = FindFaces(scale=1)
 
-def predict_energy(image):
-    print(type(image))
-    out = {'energy level': 42}
+def predict_energy(img):
+    image = cv2.imdecode(np.frombuffer(img, dtype=np.uint8), -1)
+    locations = find_faces.find_faces(image)
+    names = compare_faces.get_names(image=image, locations=locations)
+    expressions = read_expressions.get_expressions(image=image, locations=locations)
+    out = {"energy":42}
+    out['faces'] = [{"name":n, "location":l, "expression":e} for n, l, e in zip(names, locations, expressions)]
     return out
-
-
-
-
-
