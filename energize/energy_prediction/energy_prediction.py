81336c92867dcbcad660df4a86b055c1578da2f2
Faab <fabian.jansen@cblab.int>
2019-10-15 13:11:11 +0200

    Change back find_faces

--- energize/energy_prediction/energy_prediction.py
+++ energize/energy_prediction/energy_prediction.py
@@ -1,3 +1,9 @@
+from energize.energy_prediction.read_expressions import ReadExpressions
+from energize.energy_prediction.compare_faces import CompareFaces
+from energize.energy_prediction.find_faces import FindFaces
+
+find_faces = FindFaces()
+
 def predict_energy(image):
     print(type(image))
     out = {'energy level': 42}
--- energize/energy_prediction/find_faces.py
+++ energize/energy_prediction/find_faces.py
@@ -3,42 +3,19 @@ import face_recognition
 from energize.pipeline.pipeline import PipelineModule
 import numpy as np
 
-
-#class FindFaces:
-#
-#    def __init__(self, output_fnc=None, scale=1):
-#        self._output_fnc = output_fnc
-#        self.scale = 1
-#
-#    def do_shizzle(self, **kwargs):
-#        image = kwargs.pop('image')
-#        scaled_image = cv2.resize(image, (0, 0), fx=self.scale, fy=self.scale)[:, :, ::-1]
-#        scaled_locations = face_recognition.face_locations(scaled_image)
-#        locations = [list(int(l / self.scale) for l in loc) for loc in scaled_locations]
-#        self._output_fnc(image=image, locations=locations)
-
 class FindFaces(PipelineModule):
 
-    def __init__(self, next=None):
+    def __init__(self, next=None, scale=1):
         super().__init__(next)
-        self.scale = 1
+        self.scale = scale
 
     def do_shizzle(self, **kwargs):
         image = kwargs.pop('image')
-        #TODO: Check if image is 8bit gray
-        if image.max() <= 1 :
-            # Ugly way to check if image is not in right format
-            image = (image*255).astype('uint8')
+        locations = self.find_faces(image)
+        self.next.do_shizzle(image=image, locations=locations)
 
-        image = image.astype('uint8')
-        locations = face_recognition.face_locations(image)
-        faces = []
-        for face_location in locations:
-            top, right, bottom, left = face_location
-            face_image = image[top:bottom, left:right]
-            faces.append(face_image)
-
-        self.next.do_shizzle(faces=faces)
-
-        return faces
-
+    def find_faces(self, image):
+        scaled_image = cv2.resize(image, (0, 0), fx=self.scale, fy=self.scale)[:, :, ::-1]
+        scaled_locations = face_recognition.face_locations(scaled_image)
+        locations = [list(int(l / self.scale) for l in loc) for loc in scaled_locations]
+        return locations
