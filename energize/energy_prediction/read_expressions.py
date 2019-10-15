7d8900216214a3db0255a54255c7fd9280e67474
m3ct0n <chriolivier@gmail.com>
2019-10-15 15:20:45 +0200

    Incorporated prediction to the pipeline

--- energize/energy_prediction/read_expressions.py
+++ energize/energy_prediction/read_expressions.py
@@ -1,20 +1,31 @@
+import cv2
 from energize.pipeline.pipeline import PipelineModule
 
 class ReadExpressions(PipelineModule):
 
-    def __init__(self, next=None):
+    def __init__(self, next=None, model=None):
         super().__init__(next)
-        self.FER_model = None
+        self.model = model
 
     def do_shizzle(self, **kwargs):
         image = kwargs.pop('image', None)
         locations = kwargs.pop('locations', [])
         names = kwargs.pop('names', [])
         if image is not None and len(locations) > 0:
-            expressions = self.get_expressions(image, locations)
+            faces = self.get_faces(image, locations)
+            expressions = self.get_expressions(faces)
         else:
             expressions = []
         self.next.do_shizzle(image=image, locations=locations, names=names, expressions=expressions)
 
-    def get_expressions(self, image, locations):
-        return ["Unknown"]*len(locations)
+    def get_faces(self, image, face_locations):
+        faces = []
+        for face_location in face_locations:
+            top, right, bottom, left = face_location
+            face_image = image[top:bottom, left:right]
+            faces.append(face_image)
+        return faces
+
+    def get_expressions(self, faces):
+        gray_faces = [cv2.cvtColor(face, cv2.COLOR_BGR2GRAY) for face in faces]
+        return self.model.predict_from_faces(gray_faces)
