bbe71588d86172925569129b2e34663f0118d012
Faab <fabian.jansen@cblab.int>
2019-10-15 10:30:56 +0200

    Clean up commented code

--- energize/capture_video/capture_video.py
+++ energize/capture_video/capture_video.py
@@ -3,27 +3,6 @@ from PIL import ImageGrab
 import numpy as np
 from energize.pipeline.pipeline import PipelineModule
 
-#class CaptureVideo:
-#
-#    def __init__(self, output_fnc=None, source='screen'):
-#        self._output_fnc = output_fnc
-#        self.source = source
-#
-#    def do_shizzle(self):
-#        if self._output_fnc is None:
-#            raise ValueError("output_fnc cannot be None")
-#        if self.source == 'camera':
-#            source = cv2.VideoCapture(0)
-#            while True:
-#                _, im = source.read()
-#                self._output_fnc(image=im)
-#        elif self.source == 'screen':
-#            while True:
-#                im = cv2.cvtColor(np.array(ImageGrab.grab()), cv2.COLOR_RGB2BGR)
-#                self._output_fnc(image=im)
-#        else:
-#            raise NotImplementedError("Reading from file not yet supported"#
-
 class CaptureVideo(PipelineModule):
 
     def __init__(self, next=None, source='screen'):
--- energize/energy_prediction/find_faces.py
+++ energize/energy_prediction/find_faces.py
@@ -2,18 +2,6 @@ import cv2
 import face_recognition
 from energize.pipeline.pipeline import PipelineModule
 
-#class FindFaces:
-#
-#    def __init__(self, output_fnc=None, scale=1):
-#        self._output_fnc = output_fnc
-#        self.scale = 1
-#
-#    def do_shizzle(self, **kwargs):
-#        image = kwargs.pop('image')
-#        scaled_image = cv2.resize(image, (0, 0), fx=self.scale, fy=self.scale)[:, :, ::-1]
-#        scaled_locations = face_recognition.face_locations(scaled_image)
-#        locations = [list(int(l / self.scale) for l in loc) for loc in scaled_locations]
-#        self._output_fnc(image=image, locations=locations)
 
 class FindFaces(PipelineModule):
 
