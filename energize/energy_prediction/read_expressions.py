cb41f3b6e9c3d3e853f825586e1818bcc3d313d0
Faab <fabian.jansen@cblab.int>
2019-10-15 10:25:46 +0200

    Make read_expressions a pipeline module

--- energize/energy_prediction/read_expressions.py
+++ energize/energy_prediction/read_expressions.py
@@ -1,14 +1,13 @@
-import cv2
-import face_recognition
+from energize.pipeline.pipeline import PipelineModule
 
-class ReadExpressions:
+class ReadExpressions(PipelineModule):
 
-    def __init__(self, output_fnc=None):
-        self._output_fnc = output_fnc
+    def __init__(self, next=None):
+        super().__init__(next)
 
     def do_shizzle(self, **kwargs):
         image = kwargs.pop('image', None)
         locations = kwargs.pop('locations', [])
         names = kwargs.pop('names', [])
         expressions = []
-        self._output_fnc(image=image, locations=locations, names=names, expressions=expressions)
+        self.next.do_shizzle(image=image, locations=locations, names=names, expressions=expressions)
