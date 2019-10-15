9f130ac1c394c27bb5a5731837cd7239fb19597d
NedimBayrakdar <nedim.bayrakdar@ing.com>
2019-10-15 13:53:29 +0200

    model now returns list of strings (the emotions) when given a list of faces as input for predictions

--- energize/energy_prediction/find_faces.py
+++ energize/energy_prediction/find_faces.py
@@ -11,6 +11,7 @@ class FindFaces(PipelineModule):
 
     def do_shizzle(self, **kwargs):
         image = kwargs.pop('image')
+<<<<<<< HEAD
         locations = self.find_faces(image)
         self.next.do_shizzle(image=image, locations=locations)
 
@@ -19,3 +20,22 @@ class FindFaces(PipelineModule):
         scaled_locations = face_recognition.face_locations(scaled_image)
         locations = [list(int(l / self.scale) for l in loc) for loc in scaled_locations]
         return locations
+=======
+        #TODO: Check if image is 8bit gray
+        if image.max() <= 1 :
+            # Ugly way to check if image is not in right format
+            image = (image*255).astype('uint8')
+
+        image = image.astype('uint8')
+        locations = face_recognition.face_locations(image)
+        faces = []
+        for face_location in locations:
+            top, right, bottom, left = face_location
+            face_image = image[top:bottom, left:right]
+            faces.append(face_image)
+
+        # self.next.do_shizzle(faces=faces, locations = locations)
+
+        return faces
+
+>>>>>>> model now returns list of strings (the emotions) when given a list of faces as input for predictions
