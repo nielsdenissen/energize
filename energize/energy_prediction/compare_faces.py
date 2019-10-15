a44ff74ff24b50593894b967c3dcb7d20859d83d
Niels Denissen <nielsdenissen@gmail.com>
2019-10-15 15:55:08 +0200

    Catch all exceptions compare_faces

--- energize/energy_prediction/compare_faces.py
+++ energize/energy_prediction/compare_faces.py
@@ -49,16 +49,16 @@ class CompareFaces(PipelineModule):
         self.next.do_shizzle(image=image, locations=locations, names=names)
 
     def get_names(self, image, locations):
+        names = ["Unknown"] * len(locations)
         try:
             faces = np.array(face_recognition.face_encodings(image, locations, num_jitters=1))
             distances = np.linalg.norm(faces[:, np.newaxis, np.newaxis, :] - self.embeddings[np.newaxis, :, :, :], axis=3)
-            names = ["Unknown"] * len(locations)
             idx = np.unravel_index(np.nanargmin(distances), distances.shape)
             while distances[idx] < self.tolerance:
                 names[idx[0]] = self.names[idx[1]]
                 distances[idx[0], :, :] = np.NaN
                 distances[:, idx[1], :] = np.NaN
                 idx = np.unravel_index(np.nanargmin(distances), distances.shape)
-        except ValueError:
-            pass
+        except Exception as e:
+            print(f"EXCEPTION in compare_faces: {e}")
         return names
