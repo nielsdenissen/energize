4b80149c0ae3209a101d55b11b5d1525b5e10fb8
m3ct0n <chriolivier@gmail.com>
2019-10-16 13:54:42 +0200

    Fixed no expression energy bug

--- energize/energy_prediction/energy_prediction.py
+++ energize/energy_prediction/energy_prediction.py
@@ -6,4 +6,7 @@ class EnergyPrediction:
         pass
 
     def predict_energy(self, image, locations, names, faces, expressions):
-        return np.mean(expressions)
+        if len(expressions) == 0:
+            return 0
+        else:
+            return np.mean(expressions)
