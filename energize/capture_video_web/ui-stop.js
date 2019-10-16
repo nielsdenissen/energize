8be9541612caaaee1b331ebe4209ec0e6c54915a
Tjin Au Yeung <tjinauyeung@gmail.com>
2019-10-16 16:46:51 +0200

    close sockets and setup ui

--- energize/capture_video_web/ui-stop.js
+++ energize/capture_video_web/ui-stop.js
@@ -1,7 +1,9 @@
 (function() {
   function cleanup() {
     const ui = document.getElementById('energizer__ui');
-    document.querySelector('body').removeChild(ui);
+    if (ui) {
+      document.querySelector('body').removeChild(ui);
+    }
   }
   cleanup();
 })();
